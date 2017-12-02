8de8dff52ce4e350615bc80d4a433bd4ae6a6630
Taylor Vance <tvance989@gmail.com>
2017-12-01 20:45:18 -0600

    Remove unnecessary RBG demo code and add simple CMYK demo

--- demo.js
+++ demo.js
@@ -23,116 +23,105 @@ Vehicle.prototype.draw = function() {
 
 // configure classes
 
-var Red = Sandbox.extendVehicle("Red", {
+var Player = Sandbox.extendVehicle("Player", {
 	maxSpeed: 200,
+	maxForce: 20,
+	mass: 1,
+	perception: 500,
+	leeway: 50,
+	color: '#f0f',
+	size: 20
+});
+
+var Coin = Sandbox.extendVehicle("Coin", {
+	maxSpeed: 150,
 	maxForce: 5,
 	mass: 1,
-	perception: 50,
-	leeway: 10,
-	color: '#c00',
-	size: 7
-});
-
-var Green = Sandbox.extendVehicle("Green", {
-	maxSpeed: 200,
-	maxForce: 10,
-	mass: 1,
 	perception: 100,
-	leeway: 20,
-	color: '#0c0',
-	size: 4
+	leeway: 50,
+	color: '#ff0',
+	size: 10
 });
-
-var Blue = Sandbox.extendVehicle("Blue", {
-	maxSpeed: 150,
-	maxForce: 15,
-	mass: 1,
-	perception: 100,
-	leeway: 15,
-	color: '#00c',
-	size: 2
-});
-
-var Black = Sandbox.extendVehicle("Black");
 
 
 // set up vehicle instances
-var red = Sandbox.createVehicle(Red, new Vector(canvas.width / 2, canvas.height / 2));
-var greens = [];
-for (var i = 0, n = 20; i < n; i++) {
-	greens.push(Sandbox.createVehicle(Green, new Vector(Math.random() * canvas.width, Math.random() * canvas.height)));
+
+var player = Sandbox.createVehicle(Player, new Vector(canvas.width / 2, canvas.height / 2));
+
+var coins = [];
+for (var i = 0, n = 50; i < n; i++) {
+	coins.push(Sandbox.createVehicle(Coin, new Vector(Math.random() * canvas.width, Math.random() * canvas.height)));
 }
-var blues = [];
-for (var i = 0, n = 500; i < n; i++) {
-	blues.push(Sandbox.createVehicle(Blue, new Vector(Math.random() * canvas.width, Math.random() * canvas.height)));
-}
-var tBlue = Sandbox.createVehicle(Blue, new Vector(Math.random() * canvas.width, Math.random() * canvas.height)); // red's target blue
-blues.push(tBlue);
-var black = Sandbox.createVehicle(Black, new Vector(canvas.width / 2, canvas.height / 2));
 
-// special draw function for the target blue
-tBlue.draw = function() {
-	ctx.save();
-	ctx.fillStyle = '#0ff';
-	ctx.fillRect(this.position.x - 3, this.position.y - 3, 9, 9);
-	ctx.restore();
 
-	ctx.save();
-	ctx.fillStyle = '#00c';
-	ctx.fillRect(this.position.x, this.position.y, 3, 3);
-	ctx.restore();
-};
+//.draw twinkles n stuff
+//Coin.draw = function() {
+	//ctx.save();
+	//ctx.fillStyle = '#00c';
+	//ctx.fillRect(this.position.x, this.position.y, 3, 3);
+	//ctx.restore();
+//};
 
+
+// input handling
 
 var clickPos = new Vector;
 canvas.addEventListener("click", function(event){
 	clickPos.x = event.clientX;
 	clickPos.y = event.clientY;
-
-	//greens.forEach(function(green){
-		//green.applyForce(new Vector(0, -2), 1, true);
-	//});
 });
 
+var arrows = new Vector;
+window.addEventListener("keydown", function(e){
+	e = e || window.event;
 
-// steering behaviors
+	var horizontal = 0;
+	var vertical = 0;
+
+	if (e.keyCode == 37) {
+		horizontal -= 1;
+	} else if(e.keyCode == 38) {
+		vertical -= 1;
+	} else if(e.keyCode == 39) {
+		horizontal += 1;
+	} else if(e.keyCode == 40) {
+		vertical += 1;
+	}
+}, true);
+
+
+// player logic
 Sandbox.addUpdateFunction(function(){
 	var dt = Sandbox.deltaTime;
 
-	// red pursues the target blue
-	red.applyForce(red.pursue(tBlue), dt);
+	player.applyForce(player.arrive(clickPos), dt);
+});
 
-	//red2.applyForce(red2.brake().scale(5), dt);
 
-	// greens flock toward red
-	greens.forEach(function(green){
+// coin logic
+Sandbox.addUpdateFunction(function(){
+	var dt = Sandbox.deltaTime;
+
+	coins.forEach(function(coin){
 		var force = new Vector;
 
-		force = force.add(green.flock(green.neighbors(greens), 10, 5, 4).scale(5));
-		force = force.add(green.pursue(red).scale(3));
+		force = force.add(coin.flock(coin.neighbors(coins), 10, 5, 4).scale(3));
+		force = force.add(coin.pursue(player).scale(2));
 
-		green.applyForce(force, dt);
+		coin.applyForce(force, dt);
 	});
+});
 
-	// blues either evade red or flock toward mouse
-	blues.forEach(function(blue){
-		var force = new Vector;
 
-		if (blue.position.sqrDist(red.position) < Math.pow(blue.perception, 2)) {
-			force = force.add(blue.evade(red));
-		} else {
-			force = force.add(blue.flock(blue.neighbors(blues), 10, 5, 4).scale(2));
-			force = force.add(blue.arrive(clickPos));
-		}
-
-		blue.applyForce(force, dt);
-	});
+// coin spawner
+Sandbox.addUpdateFunction(function(){
+	var dt = Sandbox.deltaTime;
 });
 
 
 // render code
 Sandbox.addUpdateFunction(function(){
-	ctx.fillStyle = '#def';
+	ctx.fillStyle = '#0ff';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 	Sandbox.vehicles.forEach(function(v){
